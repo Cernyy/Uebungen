709abff0fe85a33d9bf14dab95aebbdf67a62b0e
Martin Cerny <martinkoerny@gmail.com>
2021-01-28 10:23:00 +0100

    Add project files.

---
+++ Uebungen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uebungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            
+            
+        }
+    }
+}
