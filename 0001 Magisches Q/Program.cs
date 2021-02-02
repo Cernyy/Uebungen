8f9f970bae7056b70b97c8d1b627e063d94a1255
Martin Cerny <martinkoerny@gmail.com>
2021-02-02 10:30:37 +0100

    T1L14 - Gamblers Ruin

---
+++ 0001 Magisches Q/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0001_Magisches_Q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 002_Gamblers Ruin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _002_Gamblers_Ruin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Startkapital:");
+            double startk = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zielkapital:");
+            double zielk = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wiederholungen:");
+            int wiederholungen = int.Parse(Console.ReadLine());
+
+            int gewonnen = 0;
+
+            double spielkapital = startk;
+            Random r = new Random();
+
+
+            for (int i = 0; i < wiederholungen; i++)
+            {
+                double zufall = r.NextDouble();
+                if (zufall < 0.5)
+                {
+                    gewonnen++;
+                    spielkapital++;
+                }
+                else
+                {
+                    spielkapital--;
+                }
+
+                if (spielkapital <= 0 || spielkapital >= zielk)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("\nGewonnene Spiele: {0}\nStartkapital: {1}\nEndkapital: {2}", gewonnen, startk, spielkapital);
+        }
+    }
+}
