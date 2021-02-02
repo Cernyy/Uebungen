742e17dd3c5dd9c94195dd858404881887eb203b
Martin Cerny <martinkoerny@gmail.com>
2021-02-02 11:22:30 +0100

    T1M6 - Zahlumdrehen

---
+++ 003_Zahlumdrehen/Zahlumdrehen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _003_Zahlumdrehen
+{
+    class Zahlumdrehen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Umdrehen(1234));
+        }
+
+        public static int Umdrehen(int n)
+        {
+            int umgedreht = 0;
+            while (n >= 1)
+            {
+                umgedreht *= 10;
+                umgedreht += n % 10;
+                n -= n % 10;
+                n /= 10;
+            }
+            return umgedreht;
+        }
+
+    }
+}
