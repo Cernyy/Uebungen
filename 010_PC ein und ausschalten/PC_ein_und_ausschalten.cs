925825b6ae5837268e69e37dcfb30e259f277aad
Martin Cerny <martinkoerny@gmail.com>
2021-02-04 10:05:11 +0100

    (Uhrzeit Beispiel hat noc einen Fehler)

---
+++ 010_PC ein und ausschalten/PC_ein_und_ausschalten.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _010_PC_ein_und_ausschalten
+{
+    class PC_ein_und_ausschalten
+    {
+        public static bool status = false;
+        static void Main(string[] args)
+        {
+            
+        }
+
+        /*public static bool Status()
+        {
+            //get-Methode
+        }*/
+
+        public static void Einschalten()
+        {
+            if (status == false)
+                status = true;
+            else
+            Console.WriteLine("Der PC ist bereits eingeschalten worden!");
+        }
+
+        public static void Ausschalten()
+        {
+            if (status == true)
+            
+                status = false;
+            else
+            Console.WriteLine("Der PC ist bereits ausgeschalten worden!");
+        }
+    }
+}
---
+++ 011_Uhrzeit/Uhrzeit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _011_Uhrzeit
+{
+    class Uhrzeit
+    {
+        long sec;
+
+
+        static void Main(string[] args)
+        {
+
+            Uhrzeit u1 = new Uhrzeit(12, 12, 12);
+            Uhrzeit u2 = new Uhrzeit(02, 02, 02);
+
+            Console.WriteLine(u1.sec);
+            u1.Drucken();
+            u1.Sub(u2);
+            u1.Drucken();
+        }
+
+        public Uhrzeit(int hh, int mm, int ss)
+        {
+            this.sec += hh * 360;
+            this.sec += mm * 60;
+            this.sec += ss;
+        }
+
+        public void Add(Uhrzeit t2)
+        {
+            t2.sec += this.sec;
+        }
+
+        public void Sub(Uhrzeit t2)
+        {
+            if (t2.sec > sec)
+                this.sec = t2.sec - this.sec;
+            else
+                this.sec = this.sec - t2.sec;
+        }
+
+        public void Diff(Uhrzeit t2)
+        {
+
+        }
+
+        public void Drucken()
+        {
+            int hh = 0;
+            int mm = 0;
+            int ss = 0;
+
+            long thissec = this.sec;
+
+            for (int i = 0; thissec > 360 ; hh++)
+            {
+                thissec -= 360;
+            }
+            for (int i = 0; thissec > 60; mm++)
+            {
+                thissec -= 60;
+            }
+            for (int i = 0; ss < thissec; ss++) { }
+            
+
+            Console.WriteLine("Es ist {0}:{1} Uhr und {2} Sekunden.",hh,mm,ss);
+        }
+
+    }
+}
